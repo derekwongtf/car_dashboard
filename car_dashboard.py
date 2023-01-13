8b49a6b134d2d108b809eec596fb316232c87b86
derekwongtf <113747768+derekwongtf@users.noreply.github.com>
2023-01-14 03:46:33 +0800

    Update car_dashboard.py

--- car_dashboard.py
+++ car_dashboard.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as go
-import plotly.express as px
 import streamlit as st
-from datetime import datetime
 import altair as alt
 
 #Define Functions 
